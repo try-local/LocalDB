16fb5e5d81086ecb12a36982914c68ff34cabc34
Aditya <105863819+Clyders@users.noreply.github.com>
2024-09-30 13:57:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const LocalDB = require('./db');
 
 module.exports = {
+    addNote: LocalDB.addNote.bind(LocalDB),
+    getNotes: LocalDB.getNotes.bind(LocalDB),
+    deleteNote: LocalDB.deleteNote.bind(LocalDB),
+    updateNote: LocalDB.updateNote.bind(LocalDB),
+};
+
+module.exports = {
     connect: (dbName) => new LocalDB(dbName),
 };
